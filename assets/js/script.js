45ac9eb3e7e890ed9cbee5ed28f5f220dd16ba57
Gorarth <Chris-english@hotmail.co.uk>
2022-09-29 15:02:00 +0000

    added some lets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,52 @@
-const gameArea = document.getElementById('game-area')
-const mainPage = document.getElementById('main-page')
-const generalKnowledge = document.getElementById('general-knowledge')
-const sports = document.getElementById('sports')
-const history = document.getElementById('history')
-const questionArea = document.getElementById('question-area')
-const questionCategory = document.getElementById('question-category')
-const questionText = document.getElementById('question-text')
-const answerButtons = document.getElementById('answer-buttons')
-const scoreText = document.getElementById('score')
-const endScreen = document.getElementById('end-screen')
+const gameArea = document.getElementById('game-area');
+const mainPage = document.getElementById('main-page');
+const generalKnowledge = document.getElementById('general-knowledge');
+const sports = document.getElementById('sports');
+const history = document.getElementById('history');
+const questionArea = document.getElementById('question-area');
+const questionCategory = document.getElementById('question-category');
+const questionText = document.getElementById('question-text');
+const answerButtons = document.getElementById('answer-buttons');
+const scoreText = document.getElementById('score');
+const endScreen = document.getElementById('end-screen');
 
-let currentQuestion = {}
-let score = 0
-let questionCounter = 0
+
+let currentQuestion = {};
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = []
 
 generalKnowledge.addEventListener('click', startGeneralKnowledge);
 sports.addEventListener('click', startSport);
 history.addEventListener('click', startHistory);
 
+let QUESTION = []
 async function getQuestionFromAPI() {
-    url = 'https://the-trivia-api.com/api/categories';
+    url = 'https://the-trivia-api.com/api/questions';
     let response = await fetch(url);
     if (response.ok) {
-      questions = await response.json();
+      QUESTION = await response.json();
     } else {
       alert("HTTP-Error: " + response.status);
     }
 }
 
-for(let i=0; i<20; i++) {
-    console.log(questions[i]['question'], questions[i]['correctAnswer']);
-}
+QUESTION.forEach (ques => {
+    console.log(ques['question'], ['correctAnswer']);
+    })
 
 function startGeneralKnowledge () {
     mainPage.classList.add('hide')
     questionArea.classList.remove('hide')
     score = 0;
     questionCounter = 0;
+    availableQuestions = [...questions]
     getNewQuestion();
 }
 
 function getNewQuestion () {
-    questionCounter++
+    questionCounter++;
+
 }
 
 function startSport () {
